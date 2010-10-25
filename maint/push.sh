89766efee0eef4bf3fdf1d25862d72119358b4ac
Sam Vilain <sam.vilain@catalyst.net.nz>
2010-10-25 16:12:01 +1300

    manupublish html

--- maint/push.sh
+++ maint/push.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 git fetch
-if [ -n "$(git rev-list -1 refs/heads/gh-pages..refs/remotes/origin/master)" ]
+if [ -n "$(git rev-list -1 refs/heads/gh-pages..refs/remotes/origin/website)" ]
 then
-	git update-ref refs/heads/gh-pages refs/remotes/origin/master
+	git update-ref refs/heads/gh-pages refs/remotes/origin/website
 fi
 old=$(git symbolic-ref HEAD)
 git symbolic-ref HEAD refs/heads/gh-pages
 git checkout HEAD .gitignore
 git add .
 git update-ref refs/heads/gh-pages $(echo "autopublish html" | git commit-tree `git write-tree` -p HEAD -p $old)
-git push origin HEAD:master
+git push origin HEAD:website
 git symbolic-ref HEAD $old
 git read-tree HEAD
 git checkout HEAD .gitignore
