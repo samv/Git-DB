4ad1f23dd3936f472280589c404131b9e1158e4f
Sam Vilain <sam.vilain@catalyst.net.nz>
2010-10-25 16:22:25 +1300

    push website and built pages at the same time

--- maint/push.sh
+++ maint/push.sh
@@ -10,7 +10,7 @@ git symbolic-ref HEAD refs/heads/gh-pages
 git checkout HEAD .gitignore
 git add .
 git update-ref refs/heads/gh-pages $(echo "autopublish html" | git commit-tree `git write-tree` -p HEAD -p $old)
-git push origin HEAD
+git push origin HEAD website
 git symbolic-ref HEAD $old
 git read-tree HEAD
 git checkout HEAD .gitignore
